78b1524d87b1066b5917d39dc91e2138e4c28a31
luisfff29 <luisfff29@gmail.com>
2020-03-22 02:04:39 -0500

    use HTMLParser

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 import argparse
 import requests
 import re
+from html.parser import HTMLParser
 
 
 __author__ = 'luisfff29'
@@ -23,32 +24,43 @@ def parser_argparse():
     return parser
 
 
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        tags_list = []
+        for (attr, content) in attrs:
+            if tag in ['a', 'img'] and attr in ['href', 'src']:
+                tags_list.append(content)
+        return tags_list[]
+
+
 def main():
     parser = parser_argparse()
     args = parser.parse_args()
     print(args)
 
     r = requests.get(args.url).text
-    print('\nURLS:\n')
-    list_urls = pattern_url.findall(r)
-    for x in sorted(set(list_urls)):
-        print(x)
+    parser = MyHTMLParser()
+    parser.feed(r)
+    # print('\nURLS:\n')
+    # list_urls = pattern_url.findall(r)
+    # for x in sorted(set(list_urls)):
+    #     print(x)
 
-    list_emails = pattern_email.findall(r)
-    print('\nEMAILS:\n')
-    if list_emails:
-        for x in sorted(set(list_emails)):
-            print(x)
-    else:
-        print('None')
+    # list_emails = pattern_email.findall(r)
+    # print('\nEMAILS:\n')
+    # if list_emails:
+    #     for x in sorted(set(list_emails)):
+    #         print(x)
+    # else:
+    #     print('None')
 
-    list_phones = pattern_phone.findall(r)
-    print('\nPHONE NUMBERS:\n')
-    if list_phones:
-        for x in sorted(set(list_phones)):
-            print(x)
-    else:
-        print('None')
+    # list_phones = pattern_phone.findall(r)
+    # print('\nPHONE NUMBERS:\n')
+    # if list_phones:
+    #     for x in sorted(set(list_phones)):
+    #         print(x)
+    # else:
+    #     print('None')
 
 
 if __name__ == '__main__':
